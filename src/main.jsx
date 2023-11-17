5c435f1ab33106ecdaf31d34c892cb24bc918fbc
utkarsh4084 <gps1991@gmail.com>
2023-11-17 07:26:32 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import './index.css'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <header>
+      <Link to="/">#VANLIFE</Link>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
